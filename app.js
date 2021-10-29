1ef83d316992c44e3ca883442708962efd613ed4
godspeed <shouvik.mazumdar@outlook.com>
2021-10-29 12:28:36 +0530

    Adding socket Server
    Socket Server basics

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-Hello world
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = 3000;
+
+io.on("connection", socket => {
+    console.log("a user connected :D");
+
+
+
+    socket.on("chat", msg => {
+      console.log(msg);
+      io.emit("chat message", msg);
+    });
+  });
+
+  server.listen(port, () => console.log("server running on port:" + port));
